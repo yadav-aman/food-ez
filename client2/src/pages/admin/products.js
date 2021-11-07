4e6f9a09a2bd975450386a0110e0def9d93a4d7f
Aman Yadav <42231378+yadav-aman@users.noreply.github.com>
2021-11-07 21:35:09 +0530

    fixed layout

--- client2/src/pages/admin/products.js
+++ client2/src/pages/admin/products.js
@@ -3,7 +3,6 @@ import { useState, useEffect } from 'react';
 //import { useHistory } from 'react-router';
 
 const Products = () => {
-
   const [items, setItems] = useState([]);
   const [isLoaded, setLoaded] = useState(false);
   //const [token, setToken] = useContext(UserContext);
@@ -22,45 +21,51 @@ const Products = () => {
     setLoaded(true);
   };
 
-
-  if(!isLoaded)
-  {return (
-    <div>
-      <h1>Loading data</h1>
-    </div>
-  );}
-  else
-  {return (
-    <div>
-      {!items.length?(<h1>No data</h1>):
-    (
-      items.map((item)=>(
-    <section className="text-gray-600 body-font">
-      <div className="container px-5 py-24 mx-auto">
-        <div className="flex flex-wrap -m-4">
-          <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <Link to='/' className="block relative h-48 rounded overflow-hidden">
-              <img
-                alt="ecommerce"
-                className="object-cover object-center w-full h-full block"
-                src="https://dummyimage.com/420x260"
-              />
-            </Link>
-            <div className="mt-4">
-              <h1 className="text-gray-900 text-lg tracking-widest title-font mb-1 font-bold">
-              {item.name}
-              </h1>
-              <h3 className=" text-gray-500 title-font text-xs font-medium">
-              Description: {item.description}
-              </h3>
-              <p className="mt-1">Price: ₹{item.price}/unit</p>
-            </div>
-          </div>
-        </div>
+  if (!isLoaded) {
+    return (
+      <div>
+        <h1>Loading data</h1>
       </div>
-    </section>)))}
-    </div>
-  );}
+    );
+  } else {
+    return (
+      <div>
+        {!items.length ? (
+          <h1>No data</h1>
+        ) : (
+          <section className="text-gray-600 body-font">
+            <div className="container px-5 py-24 mx-auto">
+              <div className="flex flex-wrap -m-4">
+                {items.map((item) => (
+                  <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <Link
+                      to="/"
+                      className="block relative h-48 rounded overflow-hidden"
+                    >
+                      <img
+                        alt="ecommerce"
+                        className="object-cover object-center w-full h-full block"
+                        src="https://dummyimage.com/420x260"
+                      />
+                    </Link>
+                    <div className="mt-4">
+                      <h1 className="text-gray-900 text-lg tracking-widest title-font mb-1 font-bold">
+                        {item.name}
+                      </h1>
+                      <h3 className=" text-gray-500 title-font text-xs font-medium">
+                        Description: {item.description}
+                      </h3>
+                      <p className="mt-1">Price: ₹{item.price}/unit</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </div>
+    );
+  }
 };
 
 export default Products;
