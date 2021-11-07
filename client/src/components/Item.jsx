cf270d21a50fe8997bd5fb31e6fe06e827444f77
ayushvarma7 <57842615+ayushvarma7@users.noreply.github.com>
2021-11-07 17:25:52 +0530

    changes

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -1,11 +1,12 @@
 const Item = (props) => {
     return (
       <div>
-        <h1>{props.name}</h1>
-        <h2>{props.description}</h2>
-        <h2>{props.price}</h2>
-        <h2>{props.veg}</h2>
-        <h2>{props.quantity}</h2>
+        <h1>Name of dish:- {props.name}</h1>
+        <h2>description of food:-{props.description}</h2>
+        <h2>Price per meal:-{props.price}</h2>
+        <h2>quantity left:-{props.quantity}</h2>
+        <h2>Meal type:-{props.veg ? "it's veg" : "it's non-veg"}</h2>
+        
       </div>
     );
   };
