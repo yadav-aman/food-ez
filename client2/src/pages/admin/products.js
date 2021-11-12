6c136a25f356c72f0742657c4ab5f306855eeb3e
Akansh Mittal <akanshmittal9045@gmail.com>
2021-11-12 21:04:10 +0530

    if Out of stock then link removed

--- client2/src/pages/admin/products.js
+++ client2/src/pages/admin/products.js
@@ -31,27 +31,60 @@ const Products = () => {
               <div className="flex flex-wrap -m-6 backdrop-blur-lg ">
                 {items.map((item) => (
                   <div className="lg:w-1/4 md:w-1/2 p-4 w-full border-double rounded-lg border-4 border-light-blue-500">
-                    <Link
-                      to={`/product/${item.id}`}
-                      className="block relative h-48 rounded overflow-hidden"
-                    >
+                    {item.qty > 0 ? (
+                      <Link
+                        to={`/product/${item.id}`}
+                        className="block relative h-48 rounded overflow-hidden"
+                      >
+                        <img
+                          alt="ecommerce"
+                          className={
+                            'object-cover object-center w-full h-full block ' +
+                            (item.qty <= 0 ? 'opacity-30' : 'opacity-100')
+                          }
+                          // src="https://dummyimage.com/420x260"
+                          // src="https://dummyimage.com/420x260"
+                          // src="`https://picsum.photos/200/300?random=${item.name}`"
+                          src="https://picsum.photos/200/300"
+                        />
+                      </Link>
+                    ) : (
                       <img
                         alt="ecommerce"
-                        className={"object-cover object-center w-full h-full block " +( item.qty <=0 ? "opacity-0": "opacity-100")}
+                        className={
+                          'object-cover object-center w-full block relative h-48 rounded overflow-hidden ' +
+                          (item.qty <= 0 ? 'opacity-30' : 'opacity-100')
+                        }
                         // src="https://dummyimage.com/420x260"
                         // src="https://dummyimage.com/420x260"
                         // src="`https://picsum.photos/200/300?random=${item.name}`"
                         src="https://picsum.photos/200/300"
                       />
-                    </Link>
-                    <div className={"mt-4 " + (item.qty<=0 ? " filter blur-sm" : " filter blur-none" )}>
-                      <h1 className="text-gray-900 text-lg tracking-widest title-font mb-1 font-bold">
+                    )}
+                    <div className="mt-4">
+                      <h1
+                        className={
+                          'text-gray-900 text-lg tracking-widest title-font mb-1 font-bold ' +
+                          (item.qty <= 0 ? ' opacity-30' : ' filter blur-none')
+                        }
+                      >
                         {item.name}
                       </h1>
-                      <h3 className=" text-gray-500 title-font text-xs font-medium">
-                        Quantity Left: {item.qty}
-                      </h3>
-                      <p className="mt-1">Price: ₹{item.price}/unit</p>
+                      {item.qty > 0 ? (
+                        <h2 className=" text-gray-500 title-font text-xs font-medium">
+                          Quantity Left: {item.qty}
+                        </h2>
+                      ) : (
+                        <h1 className="font-bold">Out Of Stock</h1>
+                      )}
+                      <p
+                        className={
+                          'mt-1' +
+                          (item.qty <= 0 ? ' opacity-30' : ' filter blur-none')
+                        }
+                      >
+                        Price: ₹{item.price}/unit
+                      </p>
                     </div>
                   </div>
                 ))}
