5b73d1feb1dbf73d60557af890d334f13b5ad341
Akansh Mittal <akanshmittal9045@gmail.com>
2021-11-12 15:47:37 +0530

    User Photo replaced

--- client2/src/dashboard/topnavigation/index.js
+++ client2/src/dashboard/topnavigation/index.js
@@ -51,11 +51,24 @@ export default function TopNavigation() {
           </div>
           <div className="flex items-center justify-end ml-5 p-1 relative w-1/4 sm:mr-0 sm:right-auto">
             <a href="#" className="block relative">
-              <img
+              {/* <img
                 alt="Jonathan Ilunga"
                 src="/images/9.jpg"
                 className="h-10 mx-auto object-cover rounded-full w-10"
-              />
+              /> */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className=" h-10 mx-auto object-cover rounded-full w-10"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+                  clipRule="evenodd"
+                  style={{ color: 'darkcyan' }}
+                />
+              </svg>
             </a>
           </div>
         </div>
