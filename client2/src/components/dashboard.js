a234f4b1c23feec38cb494351a56477f1a7d24c8
Akansh Mittal <akanshmittal9045@gmail.com>
2021-11-12 14:24:13 +0530

    Dashboard content added

--- client2/src/components/dashboard.js
+++ client2/src/components/dashboard.js
@@ -1,5 +1,51 @@
 import Loader from './loader';
+import InfoCard from './infocard';
+import moneyIcon from '../dashboard/sidenavigation/icons/money.svg';
+import RoundIcon from './RoundIcon';
+import LogoutIcon from '../dashboard/sidenavigation/icons/logout';
 
-const Dashboard = () => <Loader />;
+const Dashboard = () => {
+  {
+    /* <Loader />; */
+  }
+  return (
+    <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
+      <InfoCard title="Total clients" value="6389">
+        <RoundIcon
+          icon={LogoutIcon}
+          iconColorClass="text-orange-500 dark:text-orange-100"
+          bgColorClass="bg-orange-100 dark:bg-orange-500"
+          className="mr-4"
+        />
+      </InfoCard>
 
+      <InfoCard title="Account balance" value="$ 46,760.89">
+        <RoundIcon
+          icon={LogoutIcon}
+          iconColorClass="text-green-500 dark:text-green-100"
+          bgColorClass="bg-green-100 dark:bg-green-500"
+          className="mr-4"
+        />
+      </InfoCard>
+
+      <InfoCard title="New sales" value="376">
+        <RoundIcon
+          icon={LogoutIcon}
+          iconColorClass="text-blue-500 dark:text-blue-100"
+          bgColorClass="bg-blue-100 dark:bg-blue-500"
+          className="mr-4"
+        />
+      </InfoCard>
+
+      <InfoCard title="Pending contacts" value="35">
+        <RoundIcon
+          icon={LogoutIcon}
+          iconColorClass="text-teal-500 dark:text-teal-100"
+          bgColorClass="bg-teal-100 dark:bg-teal-500"
+          className="mr-4"
+        />
+      </InfoCard>
+    </div>
+  );
+};
 export default Dashboard;
