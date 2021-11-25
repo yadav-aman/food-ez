c98d22816e88c4dfa6fae12bc70222456d1d6083
Aman Yadav <42231378+yadav-aman@users.noreply.github.com>
2021-11-25 20:08:55 +0530

    updated dashboard

--- client2/src/components/dashboard.js
+++ client2/src/components/dashboard.js
@@ -21,43 +21,43 @@ const Dashboard = () => {
   }
   return (
     <>
-    <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
-      <InfoCard title="Total clients" value="6389">
-        <RoundIcon
-          icon={LogoutIcon}
-          iconColorClass="text-orange-500 dark:text-orange-100"
-          bgColorClass="bg-orange-100 dark:bg-orange-500"
-          className="mr-4"
-        />
-      </InfoCard>
+      <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
+        <InfoCard title="Total clients" value="6389">
+          <RoundIcon
+            icon={LogoutIcon}
+            iconColorClass="text-orange-500 dark:text-orange-100"
+            bgColorClass="bg-orange-100 dark:bg-orange-500"
+            className="mr-4"
+          />
+        </InfoCard>
 
-      <InfoCard title="Account balance" value="$ 46,760.89">
-        <RoundIcon
-          icon={LogoutIcon}
-          iconColorClass="text-green-500 dark:text-green-100"
-          bgColorClass="bg-green-100 dark:bg-green-500"
-          className="mr-4"
-        />
-      </InfoCard>
+        <InfoCard title="Total Sales Today" value="₹ 10,300">
+          <RoundIcon
+            icon={LogoutIcon}
+            iconColorClass="text-green-500 dark:text-green-100"
+            bgColorClass="bg-green-100 dark:bg-green-500"
+            className="mr-4"
+          />
+        </InfoCard>
 
-      <InfoCard title="New sales" value="376">
-        <RoundIcon
-          icon={LogoutIcon}
-          iconColorClass="text-blue-500 dark:text-blue-100"
-          bgColorClass="bg-blue-100 dark:bg-blue-500"
-          className="mr-4"
-        />
-      </InfoCard>
+        <InfoCard title="Orders Today" value="376">
+          <RoundIcon
+            icon={LogoutIcon}
+            iconColorClass="text-blue-500 dark:text-blue-100"
+            bgColorClass="bg-blue-100 dark:bg-blue-500"
+            className="mr-4"
+          />
+        </InfoCard>
 
-      <InfoCard title="Pending contacts" value="35">
-        <RoundIcon
-          icon={LogoutIcon}
-          iconColorClass="text-teal-500 dark:text-teal-100"
-          bgColorClass="bg-teal-100 dark:bg-teal-500"
-          className="mr-4"
-        />
-      </InfoCard>
-    </div>
+        <InfoCard title="Pending Orders" value="35">
+          <RoundIcon
+            icon={LogoutIcon}
+            iconColorClass="text-teal-500 dark:text-teal-100"
+            bgColorClass="bg-teal-100 dark:bg-teal-500"
+            className="mr-4"
+          />
+        </InfoCard>
+      </div>
 
       <PageTitle>Charts</PageTitle>
       <div className="grid gap-6 mb-8 md:grid-cols-2">
