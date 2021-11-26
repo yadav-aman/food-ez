2aaa29275c2b037307b4c755520ef0337b349cca
Akansh Mittal <akanshmittal9045@gmail.com>
2021-11-26 22:46:12 +0530

    MyOrders and Users table updated Header Footer

--- client2/src/components/MyOrders.js
+++ client2/src/components/MyOrders.js
@@ -2,6 +2,15 @@ import { useState, useEffect } from 'react';
 import { useContext } from 'react';
 import { UserContext } from '../context/UserContext';
 import Loader from './loader';
+import {
+  Table,
+  TableHeader,
+  TableCell,
+  TableFooter,
+  TableContainer,
+  Badge,
+  Button,
+} from '@windmill/react-ui';
 
 const MyOrders = () => {
   const [items, setItems] = useState([]);
@@ -44,39 +53,44 @@ const MyOrders = () => {
       <section className="container mx-auto p-6 font-mono">
         <div className="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
           <div className="w-full overflow-x-auto">
-            <table className="w-full">
-              <thead>
-                <tr className="text-md font-semibold text-center tracking-wide text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
-                  <th className="px-4 py-3">Product Name</th>
-                  <th className="px-4 py-3">Quantity</th>
-                  <th className="px-4 py-3">Total Cost</th>
-                  <th className="px-4 py-3">Order Time</th>
-                </tr>
-              </thead>
-              <tbody className="bg-white">
-                {items.map((item) => (
-                  <tr className="text-gray-700 text-center capitalize">
-                    <td className="px-4 py-3 text-ms font-semibold border">
-                      {item.product_name.name}
-                    </td>
-                    <td className="px-4 py-3 text-ms font-semibold border">
-                      {item.qty}
-                    </td>
-                    <td className="px-4 py-3 border">
-                      <div>
-                        <p className="font-semibold  text-black ">
-                          ₹{item.qty * item.product_name.price}
-                        </p>
-                      </div>
-                    </td>
-                    <td className="px-4 py-3 text-sm border">
-                      {new Date(item.created_at).toLocaleDateString()},{' '}
-                      {new Date(item.created_at).toTimeString().substring(0, 9)}
-                    </td>
+            <TableContainer className="w-full">
+              <Table>
+                <TableHeader>
+                  <tr className="text-md font-semibold text-center uppercase border-b border-gray-600">
+                    <TableCell>Product Name</TableCell>
+                    <TableCell>Quantity</TableCell>
+                    <TableCell>Total Cost</TableCell>
+                    <TableCell>Order Time</TableCell>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </TableHeader>
+                <tbody className="bg-white">
+                  {items.map((item) => (
+                    <tr className="text-gray-700 text-center capitalize">
+                      <td className="px-4 py-3 text-ms font-semibold border">
+                        {item.product_name.name}
+                      </td>
+                      <td className="px-4 py-3 text-ms font-semibold border">
+                        {item.qty}
+                      </td>
+                      <td className="px-4 py-3 border">
+                        <div>
+                          <p className="font-semibold  text-black ">
+                            ₹{item.qty * item.product_name.price}
+                          </p>
+                        </div>
+                      </td>
+                      <td className="px-4 py-3 text-sm border">
+                        {new Date(item.created_at).toLocaleDateString()},{' '}
+                        {new Date(item.created_at)
+                          .toTimeString()
+                          .substring(0, 9)}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+              <TableFooter />
+            </TableContainer>
           </div>
         </div>
       </section>
